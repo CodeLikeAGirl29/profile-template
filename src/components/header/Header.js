e095e8047870a92f143da3c1af3dbb2c565b85e9
Lindsey Howard <miss_leading009@yahoo.com>
2022-05-08 20:34:03 -0400

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -97,7 +97,7 @@ function Header(props) {
                 activeStyle={{ fontWeight: "bold" }}
                 style={{ borderRadius: 5, color: theme.text }}
               >
-                Education and Certifications
+                Education & Certs
               </NavLink>
             </li>
             <li>
@@ -108,7 +108,7 @@ function Header(props) {
                 activeStyle={{ fontWeight: "bold" }}
                 style={{ borderRadius: 5, color: theme.text }}
               >
-                Experience
+                Exp
               </NavLink>
             </li>
             <li>
@@ -130,19 +130,8 @@ function Header(props) {
                 activeStyle={{ fontWeight: "bold" }}
                 style={{ borderRadius: 5, color: theme.text }}
               >
-                Contact and Resume
+                Contact & Resume
               </NavLink>
-            </li>
-            <li>
-              <a
-                className="cr"
-                href="https://play.google.com/store/apps/details?id=com.picle.journalify"
-                tag={Link}
-                activeStyle={{ fontWeight: "bold" }}
-                style={{ borderRadius: 5, color: theme.text }}
-              >
-                Journalify
-              </a>
             </li>
             <button {...styles} onClick={changeTheme}>
               {icon}
